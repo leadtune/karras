4e9046390df0085995f0ebbf3c547d7c33f4a194
John David Eriksen <johndavid@leadtune.com>
2011-04-14 16:53:56 -0600

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,16 @@
 (defproject karras "0.4.0-SNAPSHOT"
   :description "A clojure entity framework for MongoDB"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [org.mongodb/mongo-java-driver "2.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.mongodb/mongo-java-driver "2.1"]
                  [inflections "0.4"]]  
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-clojars "0.5.0"]
-                     [autodoc "0.7.1"]
-                     [scriptjure "0.1.9"]
+                     [autodoc "0.7.1"
+                      :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]
+                     [scriptjure "0.1.9"
+                      :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]
                      [midje "0.2.0"]]
-  :namespaces [karras.core karras.collection karras.sugar karras.entity karras.validations]
+  :aot [karras.core karras.collection karras.sugar karras.entity karras.validations]
   :autodoc {:web-src-dir "http://github.com/wilkes/karras/blob/"
             :web-home "http://wilkes.github.com/karras"})
